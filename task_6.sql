dd3c9e4be7d364e6c5362e82005d432e13a3700a
mari498 <maria2006amir@gmail.com>
2025-06-30 11:09:33 +0300

    Add task_6.sql to insert multiple rows into customer table

---
+++ task_6.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    address VARCHAR(255)
+);
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
